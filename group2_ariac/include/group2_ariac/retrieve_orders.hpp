b7572b1ab7e6e6130d258ad6f100db0de27f9c61
Abhey Sharma <abhey2526@gmail.com>
2025-03-27 10:23:04 -0400

    Created Order and KittingTask classes
    This commit adds the Order, Kitting Class and its subclasses to store the info recieved on the "/ariac/orders" topic.

--- group2_ariac/include/group2_ariac/retrieve_orders.hpp
+++ group2_ariac/include/group2_ariac/retrieve_orders.hpp
@@ -1,7 +1,123 @@
+#pragma once
+
 #include "rclcpp/rclcpp.hpp"
-// #include <string>
-// include order interface
-# include "ariac_msgs/msg/order.hpp"
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstdint> 
+#include "ariac_msgs/msg/order.hpp"
+
+class Part
+{
+    public:
+        // Default Constructor
+        Part() = default;
+
+        // Constructor
+        Part(uint8_t input_color,uint8_t input_type)
+        {
+            color = input_color;
+            type = input_type;
+        }
+
+        uint8_t get_color() const {return color;}
+        uint8_t get_type() const {return type;}
+
+    private:
+        uint8_t color{};
+        uint8_t type{};
+};
+
+// kitting Part -> Part
+class KittingPart
+{
+    public:
+        // Default Constructor
+        KittingPart() = default;
+
+        // Constructor
+        KittingPart( const Part& input_part,uint8_t input_quadrant)
+        {   
+            part = input_part;
+            quadrant = input_quadrant;
+        }
+
+        const Part& get_part() const {return part;}
+        uint8_t get_quadrant() const {return quadrant;}
+
+    private:
+        Part part;
+        uint8_t quadrant{};
+
+};
+
+// Part class
+
+// kitting class
+class KittingTask
+{
+    public:
+        // Default Constructor
+        KittingTask() = default;
+
+        // Constructor
+        KittingTask(uint8_t input_agv_number,int8_t input_tray_id,uint8_t input_destination,const std::vector<KittingPart>& input_parts )
+        {
+            agv_number = input_agv_number;
+            tray_id = input_tray_id;
+            destination = input_destination;
+            parts = input_parts;
+        }
+
+        u_int8_t get_agv_number() const {return agv_number;}
+        int8_t get_tray_id() const {return tray_id;}
+        uint8_t get_destination() const {return destination;}
+        const std::vector<KittingPart>& get_parts() const {return parts;}
+
+    private:
+        uint8_t agv_number{};
+        int8_t tray_id{};
+        uint8_t destination{};
+        std::vector<KittingPart> parts;
+
+};
+
+// assembly class -> Task Class
+
+// combined class -> Task Class
+
+// Task class
+
+
+// order class
+class Order
+{
+    public:
+        //Default Constructor
+        Order() = default;
+
+        // Constructor
+        Order(std::string input_id,uint8_t input_type,bool input_priority, const KittingTask& input_kitting_task)
+        {
+            id = input_id;
+            type = input_type;
+            priority = input_priority;
+            kitting_task = input_kitting_task;
+
+        }
+
+        std::string get_id() const {return id;}
+        u_int8_t get_type() const {return type;}
+        bool get_priority() const {return priority;}
+        const KittingTask& get_kitting_task() const {return kitting_task;}
+        
+    private:
+        // fields
+        std::string id{};
+        uint8_t type{};
+        bool priority{};
+        KittingTask kitting_task;
+};
 
 class RetrieveOrders : public rclcpp::Node
 {   
@@ -13,7 +129,7 @@ class RetrieveOrders : public rclcpp::Node
                                                                             std::bind(&RetrieveOrders::order_callback, this,
                                                                             std::placeholders::_1));
 
-            RCLCPP_INFO(this->get_logger(),"subscriber created");
+            // RCLCPP_INFO(this->get_logger(),"subscriber created");
         }
 
         // subscriber callback
@@ -22,5 +138,8 @@ class RetrieveOrders : public rclcpp::Node
     private:
         // subscriber_
         rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr subscriber_;
+
+        // Storing Orders in a queue
+        std::queue<Order> orders;
 
 };
--- group2_ariac/src/retrieve_orders.cpp
+++ group2_ariac/src/retrieve_orders.cpp
@@ -2,7 +2,52 @@
 
 void RetrieveOrders::order_callback(const ariac_msgs::msg::Order::SharedPtr msg)
 {
-    RCLCPP_INFO(this->get_logger(),"callback");
+
+    // Get Order Attributes
+
+    // Create parts object
+    std::vector<KittingPart> kitting_part;
+    for(const auto& part_msg : msg->kitting_task.parts)
+    {
+        kitting_part.push_back(KittingPart(Part(part_msg.part.color,part_msg.part.type),
+                                    part_msg.quadrant));
+    }
+
+    // Create KittingTask object
+    KittingTask kitting(msg->kitting_task.agv_number,
+                        msg->kitting_task.tray_id,
+                        msg->kitting_task.destination,
+                        kitting_part);
+
+    // Create Order object
+    Order order(msg->id, 
+                msg->type,msg->priority,
+                kitting);
+
+    // Storing orders in a queue
+    orders.push(order);
+    
+    // Display order on the terminal
+    RCLCPP_INFO(this->get_logger(),"*****************************");
+
+    RCLCPP_INFO(this->get_logger(),"- Kitting Task : %s",order.get_id().c_str());
+
+    RCLCPP_INFO(this->get_logger(),"    - Priority : %d", order.get_priority());
+
+    RCLCPP_INFO(this->get_logger(),"    - Tray : %d",order.get_kitting_task().get_tray_id());
+    
+    for (const auto& part : order.get_kitting_task().get_parts()) {
+        RCLCPP_INFO(this->get_logger(), "       - Q%d: %d %d",
+            part.get_quadrant(),
+            part.get_part().get_color(), 
+            part.get_part().get_type()
+        );
+    }
+
+    RCLCPP_INFO(this->get_logger(),"    - AGV : %d", order.get_kitting_task().get_agv_number());
+
+    RCLCPP_INFO(this->get_logger(),"*****************************");
+    
 }
 
 int main(int argc, char **argv)

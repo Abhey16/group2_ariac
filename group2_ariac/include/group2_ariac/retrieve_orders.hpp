daddfb8efc091cda31c65300aa7b8aaf54b2ec17
Abhey Sharma <abhey2526@gmail.com>
2025-03-28 16:09:09 -0400

    Integerating Interface Const variables
    This commit inteferates the const variable in the ariac msg interfaces and maps uint8 values to string values.

--- group2_ariac/include/group2_ariac/retrieve_orders.hpp
+++ group2_ariac/include/group2_ariac/retrieve_orders.hpp
@@ -18,16 +18,55 @@ public:
     // Constructor
     Part(uint8_t input_color, uint8_t input_type)
     {
-        color = input_color;
-        type = input_type;
+        color = set_color(input_color);
+        type = set_type(input_type);
     }
 
-    uint8_t get_color() const { return color; }
-    uint8_t get_type() const { return type; }
+    // set color
+    std::string set_color(uint8_t input_color)
+    {
+        switch (input_color)
+        {
+        case ariac_msgs::msg::Part::RED:
+            return "RED";
+        case ariac_msgs::msg::Part::GREEN:
+            return "GREEN";
+        case ariac_msgs::msg::Part::BLUE:
+            return "BLUE";
+        case ariac_msgs::msg::Part::ORANGE:
+            return "ORANGE";
+        case ariac_msgs::msg::Part::PURPLE:
+            return "PURPLE";
+        default:
+            return "no type added";
+        }
+    }
+
+    // set Part Type
+    std::string set_type(uint8_t input_type)
+    {
+        switch (input_type)
+        {
+        case ariac_msgs::msg::Part::BATTERY:
+            return "BATTERY";
+        case ariac_msgs::msg::Part::PUMP:
+            return "PUMP";
+        case ariac_msgs::msg::Part::SENSOR:
+            return "SENSOR";
+        case ariac_msgs::msg::Part::REGULATOR:
+            return "REGULATOR";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter function
+    std::string get_color() const { return color; }
+    std::string get_type() const { return type; }
 
 private:
-    uint8_t color{};
-    uint8_t type{};
+    std::string color{};
+    std::string type{};
 };
 
 // kitting Part -> Part
@@ -41,15 +80,34 @@ public:
     KittingPart(const Part &input_part, uint8_t input_quadrant)
     {
         part = input_part;
-        quadrant = input_quadrant;
+        quadrant = set_quadrant(input_quadrant);
     }
 
+    // set quadrant
+    std::string set_quadrant(uint8_t input_quadrant)
+    {
+        switch (input_quadrant)
+        {
+        case ariac_msgs::msg::KittingPart::QUADRANT1:
+            return "QUADRANT1";
+        case ariac_msgs::msg::KittingPart::QUADRANT2:
+            return "QUADRANT2";
+        case ariac_msgs::msg::KittingPart::QUADRANT3:
+            return "QUADRANT3";
+        case ariac_msgs::msg::KittingPart::QUADRANT4:
+            return "QUADRANT4";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter function
     const Part &get_part() const { return part; }
-    uint8_t get_quadrant() const { return quadrant; }
+    std::string get_quadrant() const { return quadrant; }
 
 private:
     Part part;
-    uint8_t quadrant{};
+    std::string quadrant;
 };
 
 // Assembly Part
@@ -77,8 +135,6 @@ private:
     geometry_msgs::msg::Vector3 install_directions;
 };
 
-// Part class
-
 // kitting class
 class KittingTask
 {
@@ -94,19 +150,38 @@ public:
     {
         agv_number = input_agv_number;
         tray_id = input_tray_id;
-        destination = input_destination;
+        destination = set_destination(input_destination);
         parts = input_parts;
     }
 
+    // set destination
+    std::string set_destination(uint8_t input_destination)
+    {
+        switch (input_destination)
+        {
+        case ariac_msgs::msg::KittingTask::KITTING:
+            return "KITTING";
+        case ariac_msgs::msg::KittingTask::ASSEMBLY_FRONT:
+            return "ASSEMBLY_FRONT";
+        case ariac_msgs::msg::KittingTask::ASSEMBLY_BACK:
+            return "ASSEMBLY_BACK";
+        case ariac_msgs::msg::KittingTask::WAREHOUSE:
+            return "WAREHOUSE";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter function
     u_int8_t get_agv_number() const { return agv_number; }
     int8_t get_tray_id() const { return tray_id; }
-    uint8_t get_destination() const { return destination; }
+    std::string get_destination() const { return destination; }
     const std::vector<KittingPart> &get_parts() const { return parts; }
 
 private:
     uint8_t agv_number{};
     int8_t tray_id{};
-    uint8_t destination{};
+    std::string destination{};
     std::vector<KittingPart> parts;
 };
 
@@ -121,17 +196,36 @@ public:
                  std::vector<AssemblyPart> input_parts)
     {
         agv_numbers = input_agv_numbers;
-        station = input_station;
+        station = set_station(input_station);
         parts = input_parts;
     }
 
+    // set assembly station
+    std::string set_station(uint8_t input_station)
+    {
+        switch (input_station)
+        {
+        case ariac_msgs::msg::AssemblyTask::AS1:
+            return "AS1";
+        case ariac_msgs::msg::AssemblyTask::AS2:
+            return "AS2";
+        case ariac_msgs::msg::AssemblyTask::AS3:
+            return "AS3";
+        case ariac_msgs::msg::AssemblyTask::AS4:
+            return "AS4";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter function
     const std::vector<u_int8_t> &get_agv_numbers() const { return agv_numbers; }
-    u_int8_t get_station() const { return station; }
+    std::string get_station() const { return station; }
     const std::vector<AssemblyPart> &get_parts() const { return parts; }
 
 private:
     std::vector<u_int8_t> agv_numbers;
-    u_int8_t station;
+    std::string station;
     std::vector<AssemblyPart> parts;
 };
 
@@ -143,15 +237,34 @@ public:
 
     CombinedTask(u_int8_t input_station, std::vector<AssemblyPart> input_parts)
     {
-        station = input_station;
+        station = set_station(input_station);
         parts = input_parts;
     }
 
-    u_int8_t get_station() const { return station; }
+    // set assembly station
+    std::string set_station(uint8_t input_station)
+    {
+        switch (input_station)
+        {
+        case ariac_msgs::msg::AssemblyTask::AS1:
+            return "AS1";
+        case ariac_msgs::msg::AssemblyTask::AS2:
+            return "AS2";
+        case ariac_msgs::msg::AssemblyTask::AS3:
+            return "AS3";
+        case ariac_msgs::msg::AssemblyTask::AS4:
+            return "AS4";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter function
+    std::string get_station() const { return station; }
     const std::vector<AssemblyPart> &get_parts() const { return parts; }
 
 private:
-    u_int8_t station;
+    std::string station;
     std::vector<AssemblyPart> parts;
 };
 
@@ -171,15 +284,32 @@ public:
           const CombinedTask &input_combined_task)
     {
         id = input_id;
-        type = input_type;
+        type = set_type(input_type);
         priority = input_priority;
         kitting_task = input_kitting_task;
         assembly_task = input_assembly_task;
         combined_task = input_combined_task;
     }
 
+    // set type of task
+    std::string set_type(uint8_t input_type)
+    {
+        switch (input_type)
+        {
+        case ariac_msgs::msg::Order::KITTING:
+            return "KITTING";
+        case ariac_msgs::msg::Order::ASSEMBLY:
+            return "ASSEMBLY";
+        case ariac_msgs::msg::Order::COMBINED:
+            return "COMBINED";
+        default:
+            return "no type added";
+        }
+    }
+
+    // getter functions
     std::string get_id() const { return id; }
-    u_int8_t get_type() const { return type; }
+    std::string get_type() const { return type; }
     bool get_priority() const { return priority; }
     const KittingTask &get_kitting_task() const { return kitting_task; }
     const AssemblyTask &get_assembly_task() const { return assembly_task; }
@@ -188,7 +318,7 @@ public:
 private:
     // fields
     std::string id{};
-    uint8_t type{};
+    std::string type{};
     bool priority{};
     KittingTask kitting_task;
     AssemblyTask assembly_task;
@@ -210,7 +340,7 @@ public:
 
     // subscriber callback
     void order_callback(const ariac_msgs::msg::Order::SharedPtr msg);
-    void display_order(const Order& order);
+    void display_order(const Order &order);
 
 private:
     // subscriber_

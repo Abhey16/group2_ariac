48995950224a17470c1a425e50c76eaaa0c63e6c
ReyRoque <reyroque@umd.edu>
2025-03-30 18:47:40 -0400

    Fixed CompleteOrders

--- group2_ariac/include/group2_ariac/complete_orders.hpp
+++ group2_ariac/include/group2_ariac/complete_orders.hpp
@@ -1,30 +1,47 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
-#include <ariac_msgs/msg/agv_status.hpp>
+#include <std_srvs/srv/trigger.hpp>
 #include <ariac_msgs/msg/order.hpp>
+#include <ariac_msgs/msg/agv_status.hpp>
 #include <ariac_msgs/srv/submit_order.hpp>
 
-#include <map>
-#include <set>
+#include <unordered_map>
 #include <string>
 
-class CompleteOrders : public rclcpp::Node {
+class CompleteOrders : public rclcpp::Node
+{
 public:
-    CompleteOrders() : Node("complete_orders_node") {
-
+    CompleteOrders() : Node("complete_orders")
+    {
         order_sub_ = this->create_subscription<ariac_msgs::msg::Order>(
             "/ariac/orders", 10,
             std::bind(&CompleteOrders::order_callback, this, std::placeholders::_1));
+
+        for (int i = 1; i <= 4; ++i)
+        {
+            std::string topic = "/ariac/agv" + std::to_string(i) + "_status";
+            agv_subs_[i] = this->create_subscription<ariac_msgs::msg::AGVStatus>(
+                topic, 10,
+                [this, i](const ariac_msgs::msg::AGVStatus::SharedPtr msg)
+                {
+                    this->agv_status_callback(i, msg);
+                });
+        }
+
+        submit_client_ = this->create_client<ariac_msgs::srv::SubmitOrder>("/ariac/submit_order");
 
         RCLCPP_INFO(this->get_logger(), "Complete Orders Node Initialized");
     }
 
 private:
-    rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr order_sub_;
-    std::vector<ariac_msgs::msg::Order> orders_;    
-
     void order_callback(const ariac_msgs::msg::Order::SharedPtr msg);
-
+    void agv_status_callback(int agv_num, const ariac_msgs::msg::AGVStatus::SharedPtr msg);
     void submit_order(const std::string &order_id);
+
+    rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr order_sub_;
+    std::unordered_map<int, rclcpp::Subscription<ariac_msgs::msg::AGVStatus>::SharedPtr> agv_subs_;
+
+    rclcpp::Client<ariac_msgs::srv::SubmitOrder>::SharedPtr submit_client_;
+    std::unordered_map<int, ariac_msgs::msg::Order> orders_;
 };
--- group2_ariac/src/complete_orders.cpp
+++ group2_ariac/src/complete_orders.cpp
@@ -2,25 +2,45 @@
 
 void CompleteOrders::order_callback(const ariac_msgs::msg::Order::SharedPtr msg)
 {
-    RCLCPP_INFO(this->get_logger(), "Received Order ID: %s | Priority: %s | AGV: %d",
-                msg->id.c_str(),
-                msg->priority ? "HIGH" : "NORMAL",
-                msg->kitting_task.agv_number);
-
-    orders_.push_back(*msg);
+    orders_[msg->kitting_task.agv_number] = *msg;
 }
 
-    void CompleteOrders::submit_order(const std::string &order_id) {
+void CompleteOrders::agv_status_callback(int agv_num, const ariac_msgs::msg::AGVStatus::SharedPtr msg)
+{
+    if (msg->location == 3 && orders_.count(agv_num)) {
+        submit_order(orders_[agv_num].id);
+        orders_.erase(agv_num);  // remove order from list
+    }
+}
+
+void CompleteOrders::submit_order(const std::string &order_id)
+{
+    if (!submit_client_->wait_for_service(std::chrono::seconds(3))) {
+        RCLCPP_ERROR(this->get_logger(), "Submit order service timed out");
+        return;
     }
 
-    int main(int argc, char **argv)
-    {
-        rclcpp::init(argc, argv);
-        auto node = std::make_shared<CompleteOrders>();
-        rclcpp::executors::MultiThreadedExecutor executor;
-        executor.add_node(node);
-        executor.spin();
-    
-        rclcpp::shutdown();
-        return 0;
-    }
+    auto request = std::make_shared<ariac_msgs::srv::SubmitOrder::Request>();
+    request->order_id = order_id;
+
+    submit_client_->async_send_request(request,
+        [this, order_id](rclcpp::Client<ariac_msgs::srv::SubmitOrder>::SharedFuture future) {
+            if (future.get()->success) {
+                RCLCPP_INFO(this->get_logger(), "Submitted order: %s", order_id.c_str());
+            } else {
+                RCLCPP_WARN(this->get_logger(), "Failed to submit order: %s", order_id.c_str());
+            }
+        });
+}
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CompleteOrders>();
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
+
+    rclcpp::shutdown();
+    return 0;
+}

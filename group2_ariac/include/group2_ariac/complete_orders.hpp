94395ba66299898dbc109e784b7a44e0bf1a2498
ReyRoque <reyroque@umd.edu>
2025-03-29 23:30:56 -0400

    Merge Fixes

--- group2_ariac/include/group2_ariac/complete_orders.hpp
+++ group2_ariac/include/group2_ariac/complete_orders.hpp
@@ -53,38 +53,7 @@ private:
     std::map<int, rclcpp::Subscription<ariac_msgs::msg::AGVStatus>::SharedPtr> agv_status_subs_;
     rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr order_sub_;
 
-    void order_callback(const ariac_msgs::msg::Order::SharedPtr msg) {
-        std::string order_id = msg->id;
-        int agv_num = msg->kitting_task.agv_number;
+    void order_callback(const ariac_msgs::msg::Order::SharedPtr msg);
 
-        agv_to_order_[agv_num] = order_id;
-
-        RCLCPP_INFO(this->get_logger(),
-                    "Received order '%s' assigned to AGV%d",
-                    order_id.c_str(), agv_num);
-    }
-
-    void submit_order(const std::string &order_id) {
-        auto client = this->create_client<ariac_msgs::srv::SubmitOrder>("/ariac/submit_order");
-
-        if (!client->wait_for_service(std::chrono::seconds(5))) {
-            RCLCPP_ERROR(this->get_logger(), "Service /ariac/submit_order not available");
-            return;
-        }
-
-        auto request = std::make_shared<ariac_msgs::srv::SubmitOrder::Request>();
-        request->order_id = order_id;
-
-        auto future = client->async_send_request(request);
-
-        rclcpp::executors::SingleThreadedExecutor executor;
-        executor.add_node(shared_from_this());
-        executor.spin_until_future_complete(future);
-
-        if (future.get()->success) {
-            RCLCPP_INFO(this->get_logger(), "Order '%s' submitted successfully!", order_id.c_str());
-        } else {
-            RCLCPP_WARN(this->get_logger(), "Failed to submit order '%s'", order_id.c_str());
-        }
-    }
+    void submit_order(const std::string &order_id);
 };

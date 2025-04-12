d07a8071926af5e3255cccca7d99ccbd199de2a7
Abhey Sharma <abhey2526@gmail.com>
2025-04-12 14:28:51 -0400

    Adding tray pose detection functionality
    This commit adds a function which detects the pose of trays and display its axis.

--- group2_ariac/include/group2_ariac/tray_detector.hpp
+++ group2_ariac/include/group2_ariac/tray_detector.hpp
@@ -6,21 +6,21 @@
 #include <opencv2/opencv.hpp>
 
 
-class TrayDectector : public rclcpp::Node
+class TrayDetector : public rclcpp::Node
 {
     public:
 
-        TrayDectector(std::string node_name) : Node(node_name)
+        TrayDetector(std::string node_name) : Node(node_name)
         {
             kt_right_sub_ = this->create_subscription<sensor_msgs::msg::Image>
                                     ("/ariac/sensors/rgb_camera_kts1/rgb_image",
                                         10,
-                                        std::bind(&TrayDectector::kt_right_cb,this,std::placeholders::_1));
+                                        std::bind(&TrayDetector::kt_right_cb,this,std::placeholders::_1));
 
             kt_left_sub_ = this->create_subscription<sensor_msgs::msg::Image>
                                     ("/ariac/sensors/rgb_camera_kts2/rgb_image",
                                         10,
-                                        std::bind(&TrayDectector::kt_left_cb,this,std::placeholders::_1));
+                                        std::bind(&TrayDetector::kt_left_cb,this,std::placeholders::_1));
             
             RCLCPP_INFO(this->get_logger(),"tray detector class created");
         }
@@ -28,6 +28,7 @@ class TrayDectector : public rclcpp::Node
         void kt_right_cb(sensor_msgs::msg::Image::ConstSharedPtr img);
         void kt_left_cb(sensor_msgs::msg::Image::ConstSharedPtr img);
         void detect_aruco(cv::Mat input_img, std::string win_name);
+        void tray_pose(cv::Mat input_img, std::string win_name);
     
     private:
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr kt_right_sub_;
@@ -35,5 +36,15 @@ class TrayDectector : public rclcpp::Node
 
         cv::Mat kt_right_rgb {};
         cv::Mat kt_left_rgb {};
+
+        // intrinsic parameters
+        cv::Mat rgb_intrinsic = (cv::Mat_<double>(3,3) << 
+            343.49,0.0,320.5,
+            0.0,343.49,240.5,
+            0.0,0.0,1.0);
+
+        // distortion parameters
+        cv::Mat rgb_dis = (cv::Mat::zeros(1,5,CV_64F));
         
+      
 };
--- group2_ariac/src/tray_detector.cpp
+++ group2_ariac/src/tray_detector.cpp
@@ -1,30 +1,34 @@
 #include "tray_detector.hpp"
 
-void TrayDectector::kt_right_cb(sensor_msgs::msg::Image::ConstSharedPtr img)
+void TrayDetector::kt_right_cb(sensor_msgs::msg::Image::ConstSharedPtr img)
 {   
     kt_right_rgb = cv_bridge::toCvShare(img,"bgr8")->image;
 
     cv::imshow("Display window - right", kt_right_rgb);
 
-    TrayDectector::detect_aruco(kt_right_rgb, "right_tray");
+    TrayDetector::detect_aruco(kt_right_rgb, "right_tray");
 
-    int k = cv::waitKey(1); // Wait for a keystroke in the window
+    TrayDetector::tray_pose(kt_right_rgb, "right_tray_pose");
+
+    cv::waitKey(1); // Wait for a keystroke in the window
  
 }
 
-void TrayDectector::kt_left_cb(sensor_msgs::msg::Image::ConstSharedPtr img)
+void TrayDetector::kt_left_cb(sensor_msgs::msg::Image::ConstSharedPtr img)
 {   
     kt_left_rgb = cv_bridge::toCvShare(img,"bgr8")->image;
 
     cv::imshow("Display window - left", kt_left_rgb);
 
-    TrayDectector::detect_aruco(kt_left_rgb, "left_tray");
+    TrayDetector::detect_aruco(kt_left_rgb, "left_tray");
 
-    int k = cv::waitKey(1); // Wait for a keystroke in the window
+    TrayDetector::tray_pose(kt_left_rgb, "left_tray_pose");
+
+    cv::waitKey(1); // Wait for a keystroke in the window
  
 }
 
-void TrayDectector::detect_aruco(cv::Mat input_img,std::string win_name)
+void TrayDetector::detect_aruco(cv::Mat input_img,std::string win_name)
 {   
     cv::Mat imageCopy = input_img.clone();
     std::vector<int> ids;
@@ -41,13 +45,38 @@ void TrayDectector::detect_aruco(cv::Mat input_img,std::string win_name)
     cv::imshow(win_name, imageCopy);
 }
 
+void TrayDetector::tray_pose(cv::Mat input_img, std::string win_name)
+{   
+    cv::Mat imageCopy = input_img.clone();
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f> > corners;
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_250);
+    cv::aruco::detectMarkers(input_img, dictionary, corners, ids);
+
+    if (ids.size() > 0)
+    {    
+        cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+        
+        // output parameters
+        std::vector<cv::Vec3d> rvecs, tvecs;
+        cv::aruco::estimatePoseSingleMarkers(corners, 0.05, rgb_intrinsic, rgb_dis, rvecs, tvecs);
+
+        for(size_t i=0; i<ids.size(); i++)
+        {
+            cv::drawFrameAxes(imageCopy, rgb_intrinsic, rgb_dis, rvecs[i], tvecs[i], 0.1);
+        }
+    }
+
+    cv::imshow(win_name, imageCopy);
+}
+
 // Main function to initialize ROS node and start spinning
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv); // Initialize ROS with command-line arguments
 
     // Create a RetrieveOrders node instance
-    auto node = std::make_shared<TrayDectector>("tray_detector");
+    auto node = std::make_shared<TrayDetector>("tray_detector");
 
     rclcpp::executors::MultiThreadedExecutor executor;
 

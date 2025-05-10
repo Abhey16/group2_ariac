9722698ebb398f7df178541ee75b8490ec89c1d0
Willie000 <121785873+Willie000@users.noreply.github.com>
2025-05-10 19:44:34 -0400

    Add angle tracker

--- group2_ariac/include/group2_ariac/conveyor_tracker_node.hpp
+++ group2_ariac/include/group2_ariac/conveyor_tracker_node.hpp
@@ -13,39 +13,20 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <sensor_msgs/msg/camera_info.hpp>
 #include <cv_bridge/cv_bridge.hpp>
-#include <image_geometry/pinhole_camera_model.hpp>
-#include <geometry_msgs/msg/point_stamped.hpp>
 #include <opencv2/opencv.hpp>
-#include <group2_msgs/msg/part.hpp>
-#include <group2_msgs/msg/part_list.hpp>
+#include <unordered_set>
+#include <string>
 
 class ConveyorTracker : public rclcpp::Node {
 public:
     ConveyorTracker();
+    ~ConveyorTracker();
 
 private:
     void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
-    void depthCallback(const sensor_msgs::msg::Image::SharedPtr msg);
-    void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
-    geometry_msgs::msg::PointStamped projectPixelToWorld(int u, int v, float depth);
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr rgb_sub_;
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr depth_sub_;
-    rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_sub_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
 
-    rclcpp::Publisher<group2_msgs::msg::PartList>::SharedPtr detected_parts_pub_;
-    group2_msgs::msg::PartList detections_msg_;
-
-    rclcpp::TimerBase::SharedPtr publish_parts_timer_;
-    void publish_parts_cb();
-
-    bool first_detection_ = false;
-    double update_timer_{};
-
-    image_geometry::PinholeCameraModel camera_model_;
-    bool camera_ready_;
-    cv::Mat latest_depth_;
+    std::unordered_set<std::string> seen_objects_;
 };
--- group2_ariac/src/conveyor_tracker_node.cpp
+++ group2_ariac/src/conveyor_tracker_node.cpp
@@ -8,19 +8,15 @@ ConveyorTracker::ConveyorTracker()
     image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
         "/ariac/sensors/ceiling_conveyor_camera/rgb_image", 10,
         std::bind(&ConveyorTracker::imageCallback, this, std::placeholders::_1));
+}
 
-    // Initialize timer to run part detection
-    publish_parts_timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(100),
-        std::bind(&ConveyorTracker::publish_parts_cb, this));
-
-    // Initialize publisher for detected parts
-    detected_parts_pub_ = this->create_publisher<group2_msgs::msg::PartList>("detected_conveyor_parts", 10);        
+ConveyorTracker::~ConveyorTracker()
+{
+    cv::destroyAllWindows();
 }
 
 void ConveyorTracker::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
 {
-
     cv::Mat image;
     try {
         image = cv_bridge::toCvShare(msg, "bgr8")->image;
@@ -31,7 +27,6 @@ void ConveyorTracker::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg
 
     int image_center_x = image.cols / 2;
 
-    // parts define
     cv::Mat hsv;
     cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
 
@@ -42,159 +37,81 @@ void ConveyorTracker::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg
     cv::inRange(hsv, blue_lower, blue_upper, blue_mask);
     cv::inRange(hsv, purple_lower, purple_upper, purple_mask);
 
-    // object detect
     std::vector<std::vector<cv::Point>> contours;
+
+    // --- BLUE Detection ---
     cv::findContours(blue_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    for (const auto& c : contours) {
+    for (const auto &c : contours) {
         if (cv::contourArea(c) > 2000) {
-            cv::Moments mu = cv::moments(c);
+            cv::RotatedRect rbox = cv::minAreaRect(c);
+            cv::Point2f vertices[4];
+            rbox.points(vertices);
+            for (int i = 0; i < 4; ++i) {
+                cv::line(image, vertices[i], vertices[(i + 1) % 4], cv::Scalar(255, 0, 0), 2);
+            }
 
-            if (mu.m00 == 0) continue;
+            cv::Point2f centroid = rbox.center;
 
-            cv::Point2f centroid(mu.m10 / mu.m00, mu.m01 / mu.m00);
             if (std::abs(centroid.x - image_center_x) < 5) {
+                std::string key = "blue_" + std::to_string(static_cast<int>(centroid.x)) + "_" + std::to_string(static_cast<int>(centroid.y));
+                if (seen_objects_.find(key) == seen_objects_.end()) {
+                    seen_objects_.insert(key);
 
-                double cam_x = -0.6;
-                double cam_y = 3.63;
-                double cam_z = 1.35;
-                double obj_z = 0.875;
-                double belt_speed_ = 0.345;
+                    RCLCPP_INFO(this->get_logger(), "BLUE angle: %.2f", rbox.angle);
 
-                double y = cam_y - (cam_z - obj_z);
-
-                group2_msgs::msg::Part part_msg;
-                part_msg.color = "blue";
-                part_msg.type = "battery";
-                part_msg.location = "conveyor";
-                part_msg.position.x = cam_x;
-                part_msg.position.y = y;
-                part_msg.position.z = obj_z;
-                part_msg.initial_pos.x = cam_x;
-                part_msg.initial_pos.y = y;
-                part_msg.initial_pos.z = obj_z;
-
-                // Prediction for 1s
-                double pred_y_1 = y - belt_speed_ * 1;
-                part_msg.predicted_pos_1.x = cam_x;
-                part_msg.predicted_pos_1.y = pred_y_1;
-                part_msg.predicted_pos_1.z = obj_z;
-
-                // Prediction for 2s
-                double pred_y_2 = y - belt_speed_ * 2;
-                part_msg.predicted_pos_2.x = cam_x;
-                part_msg.predicted_pos_2.y = pred_y_2;
-                part_msg.predicted_pos_2.z = obj_z;                
-
-                detections_msg_.parts.push_back(part_msg);
-
-                // RCLCPP_INFO(this->get_logger(), "BLUE BATTERY at [%.3f, %.3f, %.3f]", cam_x, y, obj_z);
-                // predic 1s, 2s later location
-                // for (int sec = 1; sec <= 2; ++sec) {
-                //     double pred_y = y - belt_speed_ * sec;
-                //     RCLCPP_INFO(this->get_logger(),
-                //     "Prediction [%ds]: at [%.3f, %.3f, %.3f]",
-                //     sec, cam_x, pred_y, obj_z);
-                // }
+                    double cam_x = -0.6, cam_y = 3.63, cam_z = 1.35, obj_z = 0.875;
+                    double belt_speed_ = 0.345;
+                    double y = cam_y - (cam_z - obj_z);
+                    RCLCPP_INFO(this->get_logger(), "BLUE BATTERY at [%.3f, %.3f, %.3f]", cam_x, y, obj_z);
+                    for (int sec = 1; sec <= 2; ++sec) {
+                        double pred_y = y - belt_speed_ * sec;
+                        RCLCPP_INFO(this->get_logger(),
+                            "Prediction [%ds]: at [%.3f, %.3f, %.3f]",
+                            sec, cam_x, pred_y, obj_z);
+                    }
+                }
             }
         }
     }
 
+    // --- PURPLE Detection ---
     cv::findContours(purple_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    for (const auto& c : contours) {
+    for (const auto &c : contours) {
         if (cv::contourArea(c) > 2000) {
-            cv::Moments mu = cv::moments(c);
+            cv::RotatedRect rbox = cv::minAreaRect(c);
+            cv::Point2f vertices[4];
+            rbox.points(vertices);
+            for (int i = 0; i < 4; ++i) {
+                cv::line(image, vertices[i], vertices[(i + 1) % 4], cv::Scalar(0, 0, 255), 2);
+            }
 
-            if (mu.m00 == 0) continue;
-
-            cv::Point2f centroid(mu.m10 / mu.m00, mu.m01 / mu.m00);
+            cv::Point2f centroid = rbox.center;
             if (std::abs(centroid.x - image_center_x) < 5) {
+                std::string key = "purple_" + std::to_string(static_cast<int>(centroid.x)) + "_" + std::to_string(static_cast<int>(centroid.y));
+                if (seen_objects_.find(key) == seen_objects_.end()) {
+                    seen_objects_.insert(key);
 
-                double cam_x = -0.6;
-                double cam_y = 3.63;
-                double cam_z = 1.35;
-                double obj_z = 0.875;
-                double belt_speed_ = 0.345;
+                    RCLCPP_INFO(this->get_logger(), "PURPLE angle: %.2f", rbox.angle);
 
-                double y = cam_y - (cam_z - obj_z);
-
-                group2_msgs::msg::Part part_msg;
-                part_msg.color = "purple";
-                part_msg.type = "pump";
-                part_msg.location = "conveyor";
-                part_msg.position.x = cam_x;
-                part_msg.position.y = y;
-                part_msg.position.z = obj_z;
-        
-                // Prediction for 1s
-                double pred_y_1 = y - belt_speed_ * 1;
-                part_msg.predicted_pos_1.x = cam_x;
-                part_msg.predicted_pos_1.y = pred_y_1;
-                part_msg.predicted_pos_1.z = obj_z;
-
-                // Prediction for 2s
-                double pred_y_2 = y - belt_speed_ * 2;
-                part_msg.predicted_pos_2.x = cam_x;
-                part_msg.predicted_pos_2.y = pred_y_2;
-                part_msg.predicted_pos_2.z = obj_z;                
-
-                detections_msg_.parts.push_back(part_msg);
-
-                // RCLCPP_INFO(this->get_logger(), "PURPLE PUMP at [%.3f, %.3f, %.3f]", cam_x, y, obj_z);
-                // // predic 1s, 2s later location
-                // for (int sec = 1; sec <= 2; ++sec) {
-                //     double pred_y = y - belt_speed_ * sec;
-                //     RCLCPP_INFO(this->get_logger(),
-                //     "Prediction [%ds]: at [%.3f, %.3f, %.3f]",
-                //     sec, cam_x, pred_y, obj_z);
-                // }
+                    double cam_x = -0.6, cam_y = 3.63, cam_z = 1.35, obj_z = 0.875;
+                    double belt_speed_ = 0.345;
+                    double y = cam_y - (cam_z - obj_z);
+                    RCLCPP_INFO(this->get_logger(), "PURPLE PUMP at [%.3f, %.3f, %.3f]", cam_x, y, obj_z);
+                    for (int sec = 1; sec <= 2; ++sec) {
+                        double pred_y = y - belt_speed_ * sec;
+                        RCLCPP_INFO(this->get_logger(),
+                            "Prediction [%ds]: at [%.3f, %.3f, %.3f]",
+                            sec, cam_x, pred_y, obj_z);
+                    }
+                }
             }
         }
     }
-    if (first_detection_) 
-    {
-        update_timer_ = 1.0;
-    }
+
+    cv::imshow("Conveyor Tracker View", image);
+    cv::waitKey(1);
 }
 
-void ConveyorTracker::publish_parts_cb()
-{
-    update_timer_ += 0.1;
-
-    if (!first_detection_ && update_timer_ >= 1.0) 
-    {
-        detected_parts_pub_->publish(detections_msg_);
-        update_timer_ = 0;
-
-        double belt_speed_ = 0.345;
-
-        for (auto &part : detections_msg_.parts)
-        {
-            part.position = part.predicted_pos_1;
-
-            double x = part.position.x;
-            double y = part.position.y;
-            double z = part.position.z;
-
-            part.predicted_pos_1.x = x;
-            part.predicted_pos_1.y = y - belt_speed_ * 1;
-            part.predicted_pos_1.z = z;
-
-            part.predicted_pos_2.x = x;
-            part.predicted_pos_2.y = y - belt_speed_ * 2;
-            part.predicted_pos_2.z = z;
-        }
-        detections_msg_.parts.erase(
-            std::remove_if(detections_msg_.parts.begin(),
-                           detections_msg_.parts.end(),
-                           [](const group2_msgs::msg::Part &part) {
-                                return part.position.y >= 4.72;
-                            }),
-            detections_msg_.parts.end()
-        );
-    }
-}
-
-// Main
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
